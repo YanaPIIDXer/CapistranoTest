b039e4eccd4884163996624b4bf5f907f9973a2e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-20 16:04:22 +0900

    ひとまずコミット

--- src/config/deploy.rb
+++ src/config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "testapp"
+set :repo_url, "git@github.com:YanaPIIDXer/CapistranoTest.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, "main"
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/testapp"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
